04f3ed82c372214d80aa3ea4988c3f460ffaf01b
ckab04 <ciceronkapenabm@gmail.com>
2024-01-19 01:07:57 +0100

    sum of multiples

---
+++ sum-of-multiples/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+}
+
+
+fn multiple_of_number(limit: u32, number: u32) -> &[u32]{
+    
+}
