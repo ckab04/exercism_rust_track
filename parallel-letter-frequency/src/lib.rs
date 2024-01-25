004db765e9e607c1105fe2f09a1cd3f1c2647ab9
ckab04 <ciceronkapenabm@gmail.com>
2024-01-25 01:08:09 +0100

    Added a threadpool and workers to deal with the worker_count

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,6 +1,42 @@
-use std::{collections::HashMap};
+use std::{collections::HashMap, thread};
+
+struct ThreadPool{
+    workers: Vec<Worker>,
+}
+
+impl ThreadPool{
+    fn new(size: usize)-> ThreadPool{
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size{
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool{workers}
+
+    }
+}
+
+struct Worker{
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker{
+
+    fn new(id: usize) -> Worker{
+
+        let thread = thread::spawn(|| {});
+        Worker{id, thread}
+    }
+}
+
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+    let global_map = HashMap::new();
+
+
     /*
     (
         "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
@@ -9,6 +45,8 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
             _ => format!("{worker_count} workers"),
         }
     );*/
+
+    global_map
 }
 
 
